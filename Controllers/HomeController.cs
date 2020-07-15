efa675c02e2f66f0f0400c73ee00b2f8188132bf
Lou <angeloburias8@gmail.com>
2020-07-16 07:17:50 +0800

    Insertion of the CamRon Proj

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Web.Mvc;
+
+namespace IdentitySample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        [HttpGet]
+  
+        public ActionResult About()
+
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Works()
+        {
+            ViewBag.Message = "Collection of Works";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Categories()
+        {
+            ViewBag.Message = "Different Categories";
+
+            return View();
+        }
+    }
+}
